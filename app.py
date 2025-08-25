81e4b6bc393357d09780a646f892402040a2ba2f
Alexandro-K <alexandrokalindrae@gmail.com>
2025-08-25 17:18:26 +0700

    modifying app.py

--- app.py
+++ app.py
@@ -248,5 +248,4 @@ def download_users_excel():
 # ==============================
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    debug_mode = os.environ.get("FLASK_ENV") != "production"
-    app.run(host="0.0.0.0", port=port, debug=debug_mode)
+    app.run(host="0.0.0.0", port=port)
